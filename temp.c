a970debb57c7013c20eac3741be665b35db14103
Lisa Joanno <joanno.lisa@etu.unice.fr>
2016-02-11 16:32:14 +0100

    Captage des options amélioré

--- temp.c
+++ temp.c
@@ -21,18 +21,7 @@ typedef struct {
 
 typedef float **MAT;
 
-/**
-Initialisations par défaut des options du système (dans le cas où les options ne sont pas précisées par l'utilisateur).
-**/
-void init_options_par_defaut() {
-	TAILLE_MATRICE = 024; 
-	MES_AFF_CPU = 1; 
-	MES_AFF_REPUSER = 0;
-	AFF = 0; 
-	NB_EXE = 10000; 
-	ETAPES = 012345;
-	NB_THREADS = 13;
-}
+
 
 matrice* init_matrice() {
 	matrice* mat = malloc(sizeof(matrice));
@@ -63,16 +52,25 @@ void afficher_options() {
 }
 
 /**
+Initialisations par défaut des options du système (dans le cas où les options ne sont pas précisées par l'utilisateur).
+**/
+void init_options_par_defaut() {
+	TAILLE_MATRICE = 24;
+	MES_AFF_CPU = 1; 
+	MES_AFF_REPUSER = 0;
+	AFF = 0; 
+	NB_EXE = 10000; 
+	ETAPES = 12345;
+	NB_THREADS = 13;
+}
+
+/**
 Sauvegarde des options précisées par l'utilisateur dans les variables prévues à cet effet.
 **/
 void capter_options(int argc, char *argv[]) {
-
-	MES_AFF_CPU = 0; 
+	MES_AFF_CPU = 0;
 	MES_AFF_REPUSER = 0;
 	AFF = 0; 
-	NB_EXE = 10000;
-
-
 
 	int option = 0;
     //Specifying the expected options
@@ -81,8 +79,6 @@ void capter_options(int argc, char *argv[]) {
         switch (option) {
             case 's' : 
              	if (atoi(optarg) <= 9 && atoi(optarg) >= 0) {
-             		TAILLE_MATRICE = atoi(optarg);
-             	} else {
              		TAILLE_MATRICE = atoi(optarg);
              	}
                 break;
@@ -101,14 +97,10 @@ void capter_options(int argc, char *argv[]) {
             case 'e' : 
             	if (atoi(optarg) <= 6 && atoi(optarg) >= 0) {
              		ETAPES = atoi(optarg);
-             	} else {
-             		ETAPES = atoi(optarg);
              	}
                 break;
             case 't' : 
             	if (atoi(optarg) <= 6 && atoi(optarg) >= 0) {
-             		NB_THREADS = atoi(optarg);
-             	} else {
              		NB_THREADS = atoi(optarg);
              	}
                 break;
@@ -122,9 +114,9 @@ void capter_options(int argc, char *argv[]) {
 int main(int argc, char *argv[]) {
 	printf("Programme de test\n");
 
+	init_options_par_defaut();
 	if (argc == 1) {
 		printf("Il n'y a pas eu d'argument.\n");
-		init_options_par_defaut();
 	} else {
 		printf("Il y a eu des arguments.\n");
 		capter_options(argc,argv);
