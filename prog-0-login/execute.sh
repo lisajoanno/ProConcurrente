7bc9b75928016d346ea466d157c1f432d1f51680
lisajoanno <lisa.joanno@gmail.com>
2016-02-21 11:46:14 +0100

    temp.c renommé, makefile modifié, execute.sh aux normes du prof

--- prog-0-login/execute.sh
+++ prog-0-login/execute.sh
@@ -9,9 +9,6 @@
 #  -M : mesure et affichage du temps d'execution (temps de reponse utilisateur), suppression de toute trace d’execution
 #  -a : affichage de la température initiale et de la température finale pour les indices correspondant au quart supérieur gauche du tableau, pour les indices modulo 2s
 
-
 # Par defaut 
-#./main -i 100340 -e 012345 -s 024 -t 13 
+./main -i 10000 -e 012345 -s 024 -t 13 -m
 
-
-./main -i 1000 -e 012345 -s 023 -t 1 -a
